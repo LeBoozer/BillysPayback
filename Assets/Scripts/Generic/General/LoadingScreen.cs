b428e4b4c083d93d38b1ee13d21a31a0a2025430
byronworms@live.de <byronworms@live.de>
2014-11-30 10:52:58 +0100

    Re-opened repo due to a bug in unity :/

---
+++ Assets/Scripts/Generic/General/LoadingScreen.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	LoadingScreen.cs
+ * Authors:	Byron Worms
+ * Editors:	-
+ */
+using UnityEngine;
+using System.Collections;
+
+/*
+ *
+ */
+public class LoadingScreen : MonoBehaviour
+{
+	// Indicates whether a loading process is being executed currently
+	private bool 				m_isLoading = false;
+	
+	// Indicates whether the loading screen is waiting for the controller
+	private bool 				m_isWaitingForConroller = false;
+
+	// The target game-state. Will be notified after the loading has been finished.
+	private GameState 			m_targetGameState = null;
+	
+	// The target game-state-controller
+	private GameStateController m_targetConroller = null;
+
+	// (Re)-initializes the loading screen
+	public void reinitialize(GameState _targetState)
+	{
+		// Reset
+		m_isLoading = false;
+	
+		// Copy
+		m_targetGameState = _targetState;
+		m_targetConroller = _targetState.getController();
+	}
+	
+	// Will be called as soon as the loading progress has been finished
+	private void onDone()
+	{
+		// Custom loading screen?
+		if(m_targetConroller.isCustomScreen() && m_isWaitingForConroller == true)
+			return;
+	
+		// Reset
+		m_isLoading = false;
+		m_isWaitingForConroller = false;
+	
+		// Notify target
+		m_targetGameState.onEnter();
+	}
+
+	// Will be called as soon as the Gui is about to be drawn
+	void OnGUI ()
+	{	
+		// Loading?
+		if(m_isLoading == true || m_isWaitingForConroller == true)
+		{
+			// Notify controller?
+			if(m_targetConroller.isCustomScreen() == true)
+			{
+				m_isWaitingForConroller = m_targetConroller.onGui(m_isWaitingForConroller, Time.deltaTime);
+				if(m_isWaitingForConroller == false)
+					onDone();
+			}
+			else
+			{
+				// Todo
+				//GUI.DrawTexture (new Rect(0,0,Screen.width,Screen.height), loadingTexture, ScaleMode.StretchToFill);
+			}
+		}
+	}
+	
+	// Will be called at a fixed rate
+	void Update()
+	{
+		// Local variables
+		bool loading = Application.isLoadingLevel;
+	
+		// Skip unwanted combinations
+		if(m_isLoading == loading)
+			return;
+		
+		// Loading finished?
+		if(m_isLoading == true && loading == false)
+		{
+			m_isWaitingForConroller = true;
+			onDone();
+		}
+		
+		// Copy
+		m_isLoading = loading;
+	}
+}
---
+++ Assets/Scripts/Generic/General/states/GameStateMachine.cs
@@ -0,0 +1,176 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	GameStateMachine.cs
+ * Authors:	Byron Worms
+ * Editors:	-
+ */
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/*
+ * Manages the game-states
+ */
+public class GameStateMaschine : MonoBehaviour
+{
+	// The current game state
+	private GameState			m_currentGameState = null;
+	
+	// Stack of paused-game states
+	private Stack<GameState>	m_gameStateStack = new Stack<GameState>();	
+	
+	// Loading screen for state changes
+	private LoadingScreen		m_loadingScreen = null;
+	
+	// Clears the stack
+	private void _clearStack()
+	{
+		// Terminate all states within the stack
+		foreach(GameState s in m_gameStateStack)
+		{
+			// Notify game-state
+			s.onExit();
+		}
+		m_gameStateStack.Clear();
+	}
+	
+	// Setups the loading screen for the specified game-state (if required!)
+	private void _setupLoadingScreen(GameState _target)
+	{
+		// Local parameters
+		GameObject obj = null;
+		GameStateController stateCtrl = null;
+	
+		// Check parameter
+		if(_target == null)
+			return;
+			
+		// Loading screen required?
+		stateCtrl = _target.getController();
+		if(stateCtrl == null || stateCtrl.getTargetLevelName() == null || stateCtrl.getTargetLevelName().Length == 0)
+		{
+			_target.onEnter();
+			return;
+		}
+			
+		// Loading screen already available?
+		if(m_loadingScreen == null)
+		{
+			// Acquire game-object
+			obj = GameObject.FindGameObjectWithTag(Tags.TAG_LOADING_SCREEN);
+			if(obj == null)
+			{
+				// Create game-object
+				obj = new GameObject();
+				obj.tag = Tags.TAG_LOADING_SCREEN;
+				Object.DontDestroyOnLoad(obj);
+			}
+			
+			// Create loading screen
+			m_loadingScreen = Game.addComponent<LoadingScreen>();
+		}
+		
+		// (Re)-initialize loading screen
+		m_loadingScreen.reinitialize(_target);
+		
+		// Start loading
+		Application.LoadLevel(stateCtrl.getTargetLevelName());
+	}
+	
+	// Returns or updates the currently active game-state
+	public GameState ActiveState
+	{
+		get { return m_currentGameState; }
+		private set {}
+	}
+	
+	// Sets a new game-state by replacing and terminated the currently active one. All stacked ones will be deleted also
+	public void setGameState<_T>()
+		where _T : GameState, new()
+	{	
+		// Check parameter
+		if(m_currentGameState != null && typeof(_T).Equals(m_currentGameState.GetType()) == true)
+			return;
+		
+		// Terminate current state
+		if(m_currentGameState != null)
+		{
+			m_currentGameState.onLeave();
+			m_currentGameState.onExit();
+		}
+		
+		// Clear stack
+		_clearStack();
+		
+		// Create new game-state
+		m_currentGameState = new _T();
+		m_currentGameState.onInit();
+		
+		// Setup loading screen (if required)
+		_setupLoadingScreen(m_currentGameState);
+	}
+	
+	// Changes the game-state. Instead of replacing and terminating the currently active state, the current state will be pushed onto the stack for later use
+	// Returns true on success, otherwise false
+	public bool pushGameState<_T>()
+		where _T : GameState, new()
+	{
+		// Check parameter
+		if(m_currentGameState != null && typeof(_T).Equals(m_currentGameState.GetType()) == true)
+			return true;
+			
+		// Push current state
+		if(m_currentGameState != null)
+		{
+			// Notify state
+			m_currentGameState.onLeave();
+			m_gameStateStack.Push(m_currentGameState);
+		}
+		
+		// Create next game-state
+		m_currentGameState = new _T();
+		m_currentGameState.onInit();
+		m_currentGameState.onEnter();
+			
+		return true;
+	}
+	
+	// Tries to pop the currently active game-state by replacing this one with the first stack element
+	// Returns true on success, otherwise false
+	public bool popGameState()
+	{
+		// Empty stack?
+		if(m_gameStateStack.Count == 0)
+			return false;
+			
+		// Terminate current state
+		if(m_currentGameState != null)
+		{
+			m_currentGameState.onLeave();
+			m_currentGameState.onExit();
+		}
+		
+		// Pop first element
+		m_currentGameState = m_gameStateStack.Pop();
+		m_currentGameState.onEnter();
+	
+		return true;
+	}
+	
+	// Will be called once a frame
+	void Update ()
+	{
+		// Update current state
+		if (m_currentGameState != null)
+			m_currentGameState.onUpdate (Time.deltaTime);
+	}
+
+	// Will be called at a fixed rate
+	void UpdateFixed ()
+	{
+		// Update current state
+		if (m_currentGameState != null)
+				m_currentGameState.onUpdateFixed (Time.deltaTime);
+	}
+}
