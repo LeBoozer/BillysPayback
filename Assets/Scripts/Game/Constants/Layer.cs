be35ebc50b7cd760ab3f381b0310663c3d1f6d1b
byronworms@live.de <byronworms@live.de>
2014-11-30 23:09:19 +0100

    Layer
    - Added mesh: RaspberryPowerUpDummy
    - Added script: Layer
    - Added layer: Environment
    - Added layer: projectileEnemy
    - Added layer: projectilePlayer
    - Added layer: player
    - Added layer: enemy
    - Added layer: collectable
    - Configured layer-based collision detection

---
+++ Assets/Scripts/Game/Constants/Layer.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	Layer.cs
+ * Authors:	Byron Worms
+ * Editors:	-
+ */
+using UnityEngine;
+using System.Collections;
+
+/*
+ * Copy of the layer used within the unity editor. 
+ * This is needed to reference tags within the code without using any hardcoding mechanisms!
+ */
+public abstract class Layer
+{
+	public static readonly string LAYER_COLLECTABLE 		= "Collectable";
+	public static readonly string LAYER_ENEMY 				= "Enemy";
+	public static readonly string LAYER_PLAYER 				= "Player";
+	public static readonly string LAYER_PROJECTILE_PLAYER	= "ProjectilePlayer";
+	public static readonly string LAYER_PROJECTILE_ENEMY	= "ProjectileEnemy";
+	public static readonly string LAYER_ENVIROMENT 			= "Enviroment";
+}
