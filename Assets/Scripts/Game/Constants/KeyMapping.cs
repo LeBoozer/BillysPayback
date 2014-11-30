995d21b6bfcabdbca8e5be87400d373705fdccbd
byronworms@live.de <byronworms@live.de>
2014-11-30 20:06:48 +0100

    Key-mappings
    - Added key-mappings in input-manager
    - Added script: KeyMapping to access mappings

---
+++ Assets/Scripts/Game/Constants/KeyMapping.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	KeyMapping.cs
+ * Authors:	Byron Worms
+ * Editors:	-
+ */
+using UnityEngine;
+using System.Collections;
+
+/*
+ * Holds all relevant key mappings from the input manager. If you change one of this value either in the input manager or in this mapping class, adjust the other value respectively!
+ */
+public abstract class KeyMapping
+{
+	/* Constant values */
+	public static readonly string KEY_VALUE_HORIZONTAL		= "Horizontal";
+	public static readonly string KEY_VALUE_VERTICAL 		= "Vertical";
+	public static readonly string KEY_ACTION_MOVE_RIGHT 	= "Right";
+	public static readonly string KEY_ACTION_MOVE_LEFT 		= "Left";
+	public static readonly string KEY_ACTION_MOVE_UP 		= "Up";
+	public static readonly string KEY_ACTION_MOVE_DOWN 		= "Down";
+	public static readonly string KEY_ACTION_JUMP 			= "Jump";
+	public static readonly string KEY_ACTION_SHOOT 			= "Shoot";
+}
