00f547d993e7a19d0b2928d8683bfe57d77f9e59
Belzedor <Belzedor@web.de>
2014-12-01 15:47:59 +0100

    + Ordner Actor im Scripts-Ordner + Camera.cs + Player.cs

---
+++ Assets/Scripts/Game/Actors/Camera.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	Camera.cs
+ * Authors:	Raik Dankworth
+ * Editors:	-
+ */
+
+using UnityEngine;
+using System.Collections;
+
+/*
+ * Control of the camera at a target object
+ */
+public class Camera : MonoBehaviour {
+
+	#region Variable
+	public 	Transform 	m_object;
+	public  float		m_distance;
+	public  float		m_damping;
+	public  float		m_threshold;
+
+
+	#endregion
+
+	#region Start
+	// Use this for initialization
+	void Start ()
+	{
+		// init values
+		Vector3 obPos = m_object.transform.position;
+		m_distance = 15;
+		m_damping = 0.01f;
+		m_threshold = 0.0001f;
+		this.transform.position = new Vector3 (obPos.x, obPos.y, obPos.z - m_distance);
+	}
+	#endregion
+
+
+	#region Update
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// Calculate the new position of the camera
+		this.transform.position += new Vector3 (customize (m_object.transform.position.x, this.transform.position.x),
+			                                     customize (m_object.transform.position.y, this.transform.position.y),
+			                                     customize (m_object.transform.position.z, this.transform.position.z + m_distance));
+	}
+
+	// Calculate the movement of the camera 
+	private float customize(float _obPos, float _ownPos)
+	{
+		// under the threshold? -> set at the some position in this dimension
+		if (Mathf.Abs(m_damping * (_obPos - _ownPos)) < m_threshold)
+			return _obPos - _ownPos;
+
+		// damped motion
+		return m_damping * (_obPos - _ownPos);
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/Game/Actors/Player.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	Player.cs
+ * Authors:	Raik Dankworth
+ * Editors:	-
+ */
+
+using UnityEngine;
+using System.Collections;
+
+
+/*
+ * Represent the player
+ * Controll the Movement and so on
+ */
+public class Player : MonoBehaviour {
+
+	#region Variable
+
+	const float 		MAX_SPEED 			= 5f;
+	const float 		JUMP_START_SPEED	= 5f;
+	const float 		GRAVITATION 		= 2.5f;
+
+	private float 		m_speed;
+	private float 		m_fly;
+	private  bool 		m_jump;
+	private bool 		m_flyStart;
+
+	#endregion
+
+	#region Start
+	// Use this for initialization
+	void Start () 
+	{
+		m_speed 	= 0;
+		m_fly 		= 0;
+		m_jump 		= false;
+		m_flyStart 	= false;
+	}
+	#endregion
+
+	#region Update
+	// Update is called once per frame
+	void Update () 
+	{
+		// pressed keys
+		bool keyD 			= Input.GetButton 		(KeyMapping.KEY_ACTION_MOVE_RIGHT);
+		bool keyA 			= Input.GetButton 		(KeyMapping.KEY_ACTION_MOVE_LEFT);
+		bool jumpKeyDown 	= Input.GetButtonDown 	(KeyMapping.KEY_ACTION_JUMP);
+		bool jumpKey 		= Input.GetButton 		(KeyMapping.KEY_ACTION_JUMP);
+
+		// movement right&left
+		if (keyD)
+		{
+			m_speed += MAX_SPEED * 0.5f * Time.deltaTime;
+			if(m_speed > MAX_SPEED)
+				m_speed = MAX_SPEED;
+		} 
+		else if(keyA)
+		{
+			m_speed -= MAX_SPEED * 0.5f * Time.deltaTime;
+			if(m_speed <  -1 * MAX_SPEED)
+				m_speed = - 1* MAX_SPEED;
+		}
+		else
+		{
+			m_speed *= (1 - Time.deltaTime * 2);
+			if(m_speed < 0.1f && m_speed > -0.1f)
+				m_speed = 0;
+		}
+
+		// jump and fly
+		// movement high&down
+		if (jumpKeyDown && !m_jump) 
+		{
+			m_jump = true;
+			m_fly = JUMP_START_SPEED;
+		} 
+		else if (jumpKeyDown && m_jump) 
+		{
+			// flying
+			m_flyStart = true;
+			if(m_fly > 0)
+				m_fly -= GRAVITATION * Time.deltaTime;
+			else
+				m_fly -= GRAVITATION * Time.deltaTime * 0.25f;
+		} 
+		else if (m_flyStart && m_jump && jumpKey) 
+		{
+			if(m_fly > 0)
+				m_fly -= GRAVITATION * Time.deltaTime;
+			else
+				m_fly -= GRAVITATION * Time.deltaTime * 0.25f;
+		} 
+		else if (m_jump) 
+		{
+			m_fly -= GRAVITATION * Time.deltaTime;
+		} 
+		// nothing?
+		else 
+		{
+		}
+
+		// set new position
+		this.transform.position += new Vector3 (m_speed, m_fly, 0) * Time.deltaTime;
+
+		// jump/fly finished?
+		if (this.transform.position.y < 0) 
+		{
+			m_jump = false;
+			m_flyStart = false;
+			m_fly = 0;
+			this.transform.position = new Vector3 (this.transform.position.x, 0, this.transform.position.z);
+		}
+
+	}
+	#endregion
+}
