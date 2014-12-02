c1b9578d24802ca2072d9b998ebc6f0e02018ac3
byronworms@live.de <byronworms@live.de>
2014-12-02 13:38:16 +0100

    FSM
    + StateAction: SA_EnableDisableOnEvent

--- Assets/Scripts/Generic/General/FSM/FSMStateAction.cs
+++ Assets/Scripts/Generic/General/FSM/FSMStateAction.cs
@@ -1,6 +1,15 @@
-﻿using UnityEngine;
+﻿/*
+ * Project:	Billy's Payback
+ * File:	FSMStateAction.cs
+ * Authors:	Byron Worms
+ * Editors:	-
+ */
+using UnityEngine;
 using System.Collections;
 
+/*
+ * Represents the basic state-action for the FSM
+ */
 public abstract class FSMStateAction : MonoBehaviour
 {
 	// Will be called as soon as the action is being triggered on enter events of the parental state
---
+++ Assets/Scripts/Generic/General/FSM/StateActions/SA_EnableDisableOnEvent.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	SA_EnableDisableOnEvent.cs
+ * Authors:	Byron Worms
+ * Editors:	-
+ */
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * Represents the basic state-action for the FSM
+ */
+public abstract class SA_EnableDisableOnEvent : FSMStateAction
+{
+	// True to enable the target objects on entering the state. Otherwise the behaviour is the other way around.
+	public bool 			m_enableOnEnter = true;
+
+	// True to skip the onenter event
+	public bool 			m_skipOnEnter = false;
+	
+	// True to skip the onleave event
+	public bool 			m_skipOnLeave = false;
+
+	// List with the target objects
+	public List<GameObject> m_targetList = new List<GameObject>();
+	
+	// Override: FSMStateAction::onActionEnter()
+	override public void onActionEnter()
+	{
+		// Local variables
+		bool enable = m_enableOnEnter;
+		
+		// Skip?
+		if(m_skipOnEnter == true)
+			return;
+		
+		// Set
+		foreach(GameObject o in m_targetList)
+			o.SetActive(enable);
+	}
+	
+	// Override: FSMStateAction::onActionLeave()
+	override public void onActionLeave()
+	{
+		// Local variables
+		bool enable = !m_enableOnEnter;
+		
+		// Skip?
+		if(m_skipOnLeave == true)
+			return;
+		
+		// Set
+		foreach(GameObject o in m_targetList)
+			o.SetActive(enable);
+	}
+}
