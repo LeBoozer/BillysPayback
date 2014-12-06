6f24da9fe878a11910e249d92fb3ab05242fdcce
byronworms@live.de <byronworms@live.de>
2014-12-07 00:41:36 +0100

    Spielercharakteristik (SK), FSM
    + Basisklasse für SK hinzugefügt: PlayerCharacteristics
    + Neues SK-Skript: PlayerCharacteristicsOnDestroy
    + Neue Funktion in FSMEventHighjack: OnDestroy
    # 'PlayerCharacteristicsWay' nutzt neue Basisklasse

---
+++ Assets/Scripts/Game/Helper/PlayerCharacteristicsOnDestroy.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	PlayerCharacteristicsOnDestroy.cs
+ * Authors:	Byron Worms
+ * Editors:	-
+ */
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * Updates the player characteristics on "OnDestroy" events
+ */
+public class PlayerCharacteristicsOnDestroy : PlayerCharacteristics
+{
+	// Override: MonoBehaviour::Start()
+	void Start()
+	{
+		// Local variables
+		FSMEventHighjack hj = null;
+	
+		// High-jack all objects
+		foreach(GameObject obj in m_objectList)
+		{
+			hj = obj.gameObject.AddComponent<FSMEventHighjack>();
+			hj.FSMOnDestroy += () => 
+			{
+				// Remove from list
+				m_objectList.Remove(obj);
+				if(m_objectList.Count <= 0)
+				{
+					onApplyCharacteristics();
+					if(gameObject != null)
+						gameObject.SetActive(false);
+				}
+			};
+		}
+	}
+	
+	// Override: MonoBehaviour::OnDestroy()
+	void OnDestroy()
+	{
+		// Local variables
+		FSMEventHighjack hj = null;
+	
+		// Remove high-jack
+		foreach(GameObject obj in m_objectList)
+		{
+			if(obj == null)
+				continue;
+			hj = obj.GetComponent<FSMEventHighjack>();
+			if(hj != null)
+				GameObject.Destroy(hj);
+		}
+	}
+}
