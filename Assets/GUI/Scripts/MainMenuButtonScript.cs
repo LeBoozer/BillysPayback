249225a2595479454c736dc0c687019119daac23
Dominique <Globox1@gmx.net>
2014-12-22 00:15:14 +0100

    * UI-Layout korrigiert * Script zur Handhabung der Button-Events im Hauptmenü hinzugefügt (Verbindung zu Buttons bereits hergestellt, aber bisher noch ungelöstes Problem, dass keine Interaktion mit den Buttons möglich)

---
+++ Assets/GUI/Scripts/MainMenuButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartButtonClicked() {
+		Debug.Log ("Startbutton was clicked!");
+	}
+
+	public void OptionsButtonClicked() {
+		Debug.Log ("Optionsbutton was clicked!");
+	}
+
+	public void CloseButtonClicked() {
+		Debug.Log ("Closebutton was clicked!");
+
+		//Application.Quit ();
+	}
+}
