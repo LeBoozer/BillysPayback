5fbc8be83341fe109bfebce0428afeaf16210b91
byronworms@live.de <byronworms@live.de>
2015-03-19 15:34:56 +0100

    FSM, Prefab, Level
    + T_OnInvalidDistance: Auslösen bei Unter/Überschreitung wahlweise
    abstellbar
    + FSM->State->Action: Neue Aktion: SA_SaveObjectTransform
    + FSM->State->Action: Neue Aktion: SA_RestoreObjectTransform
    
    # Level 3.1: Vögel verloren, neustart der Verfolgung
    # Level 3.1: Vögel angegriffen,  neustart der Verfolgung (Angriff des
    Vogels ausstehend)
    # Level 3.1: Oberer Questgeben hat Charactercontroller
    # Prefab->Deadtree: Anstatt von Meshcollidern zwei Simple Shape Collider

---
+++ Assets/Scripts/Game/Helper/FSM/RestoreObjectTransform.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	RestoreObjectTransform.cs
+ * Authors:	Byron Worms
+ * Editors:	-
+ */
+using UnityEngine;
+using System.Collections;
+
+/*
+ * ToDo
+ */
+public class RestoreObjectTransform : MonoBehaviour
+{
+    // The saved object transforms
+    public SA_SaveObjectTransform m_savedTransforms = null;
+
+    // Override: MonoBehaviour::Start()
+	void Start() 
+    {
+        Debug.Log("Start");
+
+	    // Check
+        if (m_savedTransforms == null || m_savedTransforms.m_objects == null || m_savedTransforms.m_objects.Length == 0)
+            return;
+
+        // Restore transforms
+        for (int i = 0; i < m_savedTransforms.m_objects.Length; ++i)
+        {
+            if (m_savedTransforms.m_objects[i] != null && m_savedTransforms.m_objects[i].m_transform != null)
+            {
+                m_savedTransforms.m_objects[i].m_object.transform.localPosition = m_savedTransforms.m_objects[i].m_transform.m_localPosition;
+                m_savedTransforms.m_objects[i].m_object.transform.localScale = m_savedTransforms.m_objects[i].m_transform.m_localScale;
+                m_savedTransforms.m_objects[i].m_object.transform.localRotation = m_savedTransforms.m_objects[i].m_transform.m_localRotation;
+            }
+        }
+	}
+}
