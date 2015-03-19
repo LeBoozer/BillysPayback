434d609e32dac1a29eb4626b64eac4843849b1ac
Belzedor <Belzedor@web.de>
2015-03-19 10:45:36 +0100

    # Kapitel 3.2 getrennte Wege bearbeitet ---# Antonio wartet vor der Brücke bis der Spieler ihm gefolgt ist ---# Brücke wird hinter Antonio abgebrochen ---# Brücke wird zerstört und Antonio in den Bosskampfarena gesetzt, sobald der Spieler das Tor nach BS durchlaufen hat # HuntingEnemy drehen sich immer in die Richtung des Spielers

--- Assets/Scripts/Game/Scenes/3.2/AntonioChange.cs
+++ Assets/Scripts/Game/Scenes/3.2/AntonioChange.cs
@@ -1,19 +1,44 @@
-﻿using UnityEngine;
+﻿/*
+ * Project:	Billy's Payback
+ * File:	AntonioChange.cs
+ * Authors:	Raik Dankworth
+ */
+using UnityEngine;
 using System.Collections;
 
+/**
+ * Special Trigger for Level 3.2
+ * Set Antonio to boss fight area
+ * Remove and Add important components
+ */
 public class AntonioChange : MonoBehaviour
 {
+    public GameObject m_brigde;
+    public GameObject m_antonioBossFightArea;
 
     void OnTriggerEnter(Collider _other)
     {
         // ignore all other game objects
-        if (_other.tag != Tags.TAG_COMPANION)
+        if (_other.tag != Tags.TAG_PLAYER)
             return;
 
-        //
-        GameObject _antonio = _other.gameObject;
+        // add and remove components
+        GameObject _antonio = GameObject.FindGameObjectWithTag(Tags.TAG_COMPANION);
+        if (_antonio == null)
+        { 
+            Debug.LogWarning("AntonioChange: Antonio dont found!");
+            return;
+        }
         _antonio.GetComponent<Antonio>().enabled = false;
         _antonio.AddComponent("BossAntonio");
+
+        // set antonio to the boss fight area
+        if(m_antonioBossFightArea != null)
+            _antonio.transform.position = m_antonioBossFightArea.transform.position;
+
+        // destroy the bridge
+        if(m_brigde != null)
+            Destroy(m_brigde);
     }
 
 }
