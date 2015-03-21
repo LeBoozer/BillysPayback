7353c4d4a1f03cd239c224655a287ae896b71f49
byronworms@live.de <byronworms@live.de>
2015-03-21 22:43:33 +0100

    Port, Prefab
    + Erste Vorbereitungsanweisungen für den Port (Window -> Port to Unity5)
    
    # Prefab->SimpleSmokeExplosion: Partikel werden nach Distanz sortiert
    # Prefab->ShiningDestroyableEgg: Partikeleffekt, Farbe wie normales Ei

---
+++ Assets/Editor/FindMeshColliderMeshesWithTrigger.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Project:	Billy's Payback
+ * File:	FindMeshColliderMeshesWithTrigger.cs
+ * Authors:	Byron Worms
+ * Editors:	-
+ */
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Linq;
+
+/*
+ * ToDo
+ */
+public class FindMeshColliderMeshesWithTrigger : EditorWindow
+{
+    // Add the window to the unity menu bar
+    [MenuItem("Window/Port to Unity5")]
+    static void Init()
+    {
+        // Add window
+        FindMeshColliderMeshesWithTrigger window = (FindMeshColliderMeshesWithTrigger)EditorWindow.GetWindow(typeof(FindMeshColliderMeshesWithTrigger));
+        window.minSize = new Vector2(400, 200);
+
+        // Show window
+        window.Show();
+    }
+
+    // Override: MonoBehaviour::OnGUI()
+    void OnGUI()
+    {
+        // Add label
+        GUILayout.Label("Attention should be paid to the following list during the port to Unity5:", EditorStyles.boldLabel);
+
+        // Add label
+        GUILayout.Label("# All lights can cast dynamic shadows now.", EditorStyles.label);
+        GUILayout.Space(10);
+
+        // Add button: find all triangle meshes with mesh colliders, which are configurated as trigger shapes
+        GUILayout.Label("# Meshes with triangle colliders as trigger (not supported):", EditorStyles.label);
+        if(GUILayout.Button("Find meshes"))
+        {
+            Selection.objects = FindObjectsOfType<MeshCollider>().Where(mc => mc.isTrigger && !mc.convex).Select(mc => mc.gameObject).ToArray();
+        }
+    }
+}
