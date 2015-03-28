9b7493c83dd99c88b3492be89d5f2c397a047238
Belzedor <Belzedor@web.de>
2015-03-28 13:48:25 +0100

    # BridgeScript überarbeitet für eine bessere Performance

---
+++ Assets/Scripts/Game/Items/BrigdeScriptHelper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrigdeScriptHelper : MonoBehaviour {
+
+    private bool        m_isTrigger = true;
+    private bool        m_onTrigger = false;
+    private Collider    m_ownCollider;
+
+    void Awake()
+    {
+        m_ownCollider = this.GetComponent<Collider>();
+    }
+
+	
+    void OnTriggerEnter(Collider other)
+    {
+        m_onTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // set trigger value
+        m_onTrigger = false;
+        if(m_ownCollider != null)
+            m_ownCollider.isTrigger = m_isTrigger;
+    }
+
+    public void setIsTrigger(bool _newIsTriggerValue)
+    {
+        // the some value?
+        if (m_isTrigger == _newIsTriggerValue)
+            return;
+        m_isTrigger = _newIsTriggerValue;
+
+        // dont trigger at this time?
+        if (!m_onTrigger && m_ownCollider != null)
+            m_ownCollider.isTrigger = m_isTrigger;
+    }
+}
